054154d54ef25f6e09f55c55d8f435b3d5e76543
Deivid Mladenov <dmladenov@zingasoft.com>
2021-01-10 01:27:34 +0200

    feat: implemented initial crud operations for user
    issue #4

--- company-vacation-calendar-server/api/api-routing.js
+++ company-vacation-calendar-server/api/api-routing.js
@@ -4,6 +4,8 @@ const Role = require("../helpers/roles");
 const authorize = require("../helpers/authorize");
 const userService = require("./auth/users-service");
 
+// Auth
+
 router.post("/authenticate", (req, res, next) => {
   userService
     .authenticate(req.body)
@@ -12,6 +14,29 @@ router.post("/authenticate", (req, res, next) => {
         ? res.json(user)
         : res.status(400).json({ message: "Wrong credentials!" })
     )
+    .catch((err) => next(err));
+});
+
+// Users
+
+router.get("/users/getById", (req, res, next) => {
+  userService
+    .getUserById(req.query)
+    .then((user) => res.json(user))
+    .catch((err) => next(err));
+});
+
+router.get("/users/getAll", (req, res, next) => {
+  userService
+    .getAllUsers(req.body)
+    .then((users) => res.json(users))
+    .catch((err) => next(err));
+});
+
+router.post("/user/create", (req, res, next) => {
+  userService
+    .createUser(req.body)
+    .then((id) => res.json(id))
     .catch((err) => next(err));
 });
 
--- company-vacation-calendar-server/api/auth/users-service.js
+++ company-vacation-calendar-server/api/auth/users-service.js
@@ -1,39 +1,142 @@
 const jwt = require("jsonwebtoken");
 const config = require("../../config.json");
 const Role = require("../../helpers/roles");
+const {
+  firestore
+} = require("../../helpers/firebase");
+const {
+  v4: uuidv4
+} = require("uuid");
 
-// users hardcoded for simplicity, store in a db for production applications
-const users = [
-  {
-    id: 1,
-    username: "admin",
-    password: "admin",
-    firstName: "Simeon",
-    lastName: "Mechkov",
-    role: Role.Admin,
-  },
-  {
-    id: 2,
-    username: "user",
-    password: "user",
-    firstName: "Normal",
-    lastName: "User",
-    role: Role.User,
-  },
-];
+async function authenticate({
+  email,
+  password
+}) {
+  const userQuery = await firestore.collection("users").where('email', '==', email).get();
 
-async function authenticate({ username, password }) {
-  const user = users.find(
-    (u) => u.username === username && u.password === password
-  );
-  if (user) {
-    const token = jwt.sign({ sub: user.id, role: user.role }, config.secret);
-    const { password, ...userWithoutPassword } = user;
-    return {
-      ...userWithoutPassword,
-      token,
-    };
+  let user = null;
+  userQuery.forEach(function (currentUser) {
+    user = currentUser.data();
+  });
+
+  if (user !== null) {
+    if (user.password == password) {
+      const token = jwt.sign({
+          sub: user.id,
+          role: user.role,
+        },
+        config.secret
+      );
+      const {
+        password,
+        ...userWithoutPassword
+      } = user;
+      return {
+        ...userWithoutPassword,
+        token,
+      };
+    }
+  }
+
+  return null;
+}
+
+// Users
+
+async function getUserByEmail({
+  email
+}) {
+  try {
+    const userQuery = await firestore.collection("users").where(u => u.email == email).get();
+    const user = userQuery.data();
+
+    if (user.password === password) {
+      const {
+        password,
+        ...userWithoutPassword
+      } = user;
+
+      return {
+        ...userWithoutPassword,
+      };
+    }
+
+    return null;
+  } catch (error) {
+    console.log(error);
   }
 }
 
-module.exports = { authenticate };
+async function getUserById({
+  id
+}) {
+  try {
+    const userQuery = await firestore.collection("users").doc(id).get();
+    const user = userQuery.data();
+    const {
+      password,
+      ...userWithoutPassword
+    } = user;
+
+    return {
+      ...userWithoutPassword,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getAllUsers() {
+  try {
+    const users = [];
+    const usersQuery = await firestore.collection("users").get();
+
+    usersQuery.forEach(function (user) {
+      const currentUser = user.data();
+      const {
+        password,
+        ...userWithoutPassword
+      } = currentUser;
+      users.push(userWithoutPassword);
+    });
+
+    return {
+      users,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function createUser({
+  email,
+  password,
+  firstName,
+  lastName,
+  role
+}) {
+  try {
+    let id = uuidv4();
+    await firestore.collection("users").doc(id).set({
+      id,
+      email,
+      password,
+      firstName,
+      lastName,
+      role,
+    });
+
+    return {
+      id,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  authenticate,
+  createUser,
+  getAllUsers,
+  getUserById
+};
