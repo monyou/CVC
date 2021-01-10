c76b638f4aa9d91a0f8e7977ebe59a25670a2d37
Deivid Mladenov <dmladenov@zingasoft.com>
2021-01-10 12:34:14 +0200

    feat(server): minor fixies on initial crud and created controller based architecture
    issue #4

--- company-vacation-calendar-server/api/api-routing.js
+++ company-vacation-calendar-server/api/api-routing.js
@@ -1,43 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const Role = require("../helpers/roles");
-const authorize = require("../helpers/authorize");
-const userService = require("./auth/users-service");
+const authController = require("./auth/auth-controller");
+const userController = require("./user/user-controller");
 
-// Auth
-
-router.post("/authenticate", (req, res, next) => {
-  userService
-    .authenticate(req.body)
-    .then((user) =>
-      user
-        ? res.json(user)
-        : res.status(400).json({ message: "Wrong credentials!" })
-    )
-    .catch((err) => next(err));
-});
-
-// Users
-
-router.get("/users/getById", (req, res, next) => {
-  userService
-    .getUserById(req.query)
-    .then((user) => res.json(user))
-    .catch((err) => next(err));
-});
-
-router.get("/users/getAll", (req, res, next) => {
-  userService
-    .getAllUsers(req.body)
-    .then((users) => res.json(users))
-    .catch((err) => next(err));
-});
-
-router.post("/user/create", (req, res, next) => {
-  userService
-    .createUser(req.body)
-    .then((id) => res.json(id))
-    .catch((err) => next(err));
-});
+router.use("/", authController);
+router.use("/", userController);
 
 module.exports = router;
---
+++ company-vacation-calendar-server/api/user/user-service.js
@@ -0,0 +1,78 @@
+const {
+  firestore
+} = require("../../helpers/firebase");
+const {
+  v4: uuidv4
+} = require("uuid");
+
+async function getUserById({
+  id
+}) {
+  try {
+    const userQuery = await firestore.collection("users").doc(id).get();
+    const user = userQuery.data();
+    const {
+      password,
+      ...userWithoutPassword
+    } = user;
+
+    return {
+      ...userWithoutPassword,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getAllUsers() {
+  try {
+    const usersQuery = await firestore.collection("users").get();
+    const users = usersQuery.docs.map(d => {
+      let user = d.data();
+      const {
+        password,
+        ...userWithoutPassword
+      } = user;
+
+      return userWithoutPassword;
+    })
+
+    return {
+      users,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function createUser({
+  email,
+  password,
+  firstName,
+  lastName,
+  role
+}) {
+  try {
+    let id = uuidv4();
+    await firestore.collection("users").doc(id).set({
+      id,
+      email,
+      password,
+      firstName,
+      lastName,
+      role,
+    });
+
+    return {
+      id,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  createUser,
+  getAllUsers,
+  getUserById
+};
