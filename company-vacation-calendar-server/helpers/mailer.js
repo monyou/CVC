51e782bf124239598b7be2c4ae926875284a558d
Deivid Mladenov <dmladenov@zingasoft.com>
2021-01-13 11:34:22 +0200

    feat(server): implemented activate profile functionality and fixed some flow issues
    issue #8

--- company-vacation-calendar-server/helpers/mailer.js
+++ company-vacation-calendar-server/helpers/mailer.js
@@ -1,4 +1,7 @@
 const nodemailer = require('nodemailer');
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 const transporter = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE_PROVIDER,
