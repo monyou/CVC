62d9316bc0de4fbbb098c38abc054513a8a388ee
Симеон Мечков <montishak@gmail.com>
2021-01-28 02:30:35 +0200

    feat(server): added getAllUsersByCompanyId, soft delete user

--- company-vacation-calendar-server/api/user/user-controller.js
+++ company-vacation-calendar-server/api/user/user-controller.js
@@ -5,39 +5,93 @@ const roles = require("../../helpers/roles");
 const userService = require("./user-service");
 
 router.get("/user/getById", authorize(), (req, res, next) => {
-    userService
-        .getUserById(req.query)
-        .then((user) => user ? res.json(user) : res.status(400).json({
-            message: "Unable to get user by id!"
-        }))
-        .catch((err) => next(err));
+  userService
+    .getUserById(req.query)
+    .then((user) =>
+      user
+        ? res.json(user)
+        : res.status(400).json({
+            message: "Unable to get user by id!",
+          })
+    )
+    .catch((err) => next(err));
 });
+
+router.get(
+  "/user/getAllByCompanyId",
+  authorize([roles.SuperAdmin, roles.Admin]),
+  (req, res, next) => {
+    userService
+      .getUsersByCompanyId(req.query)
+      .then((users) =>
+        users
+          ? res.json(users)
+          : res.status(400).json({
+              message: "Unable to get users by company id!",
+            })
+      )
+      .catch((err) => next(err));
+  }
+);
 
 router.get("/user/getAll", authorize([roles.SuperAdmin]), (req, res, next) => {
-    userService
-        .getAllUsers()
-        .then((users) => users ? res.json(users) : res.status(400).json({
-            message: "Unable to get all users!"
-        }))
-        .catch((err) => next(err));
+  userService
+    .getAllUsers()
+    .then((users) =>
+      users
+        ? res.json(users)
+        : res.status(400).json({
+            message: "Unable to get all users!",
+          })
+    )
+    .catch((err) => next(err));
 });
 
-router.post("/user/create", authorize([roles.Admin, roles.SuperAdmin]), (req, res, next) => {
+router.post(
+  "/user/create",
+  authorize([roles.Admin, roles.SuperAdmin]),
+  (req, res, next) => {
     userService
-        .createUser(req.body)
-        .then((id) => id ? res.json(id) : res.status(400).json({
-            message: "Unable to create user!"
-        }))
-        .catch((err) => next(err));
-});
+      .createUser(req.body)
+      .then((id) =>
+        id
+          ? res.json(id)
+          : res.status(400).json({
+              message: "Unable to create user!",
+            })
+      )
+      .catch((err) => next(err));
+  }
+);
+
+router.post(
+  "/user/delete",
+  authorize([roles.Admin, roles.SuperAdmin]),
+  (req, res, next) => {
+    userService
+      .deleteUser(req.body)
+      .then((response) =>
+        response
+          ? res.json(response)
+          : res.status(400).json({
+              message: "Unable to create user!",
+            })
+      )
+      .catch((err) => next(err));
+  }
+);
 
 router.put("/user/activate", (req, res, next) => {
-    userService
-        .activateUser(req.body)
-        .then((result) => result ? res.json(result) : res.status(400).json({
-            message: "Unable to activate user!"
-        }))
-        .catch((err) => next(err));
+  userService
+    .activateUser(req.body)
+    .then((result) =>
+      result
+        ? res.json(result)
+        : res.status(400).json({
+            message: "Unable to activate user!",
+          })
+    )
+    .catch((err) => next(err));
 });
 
 module.exports = router;
--- company-vacation-calendar-server/api/user/user-service.js
+++ company-vacation-calendar-server/api/user/user-service.js
@@ -1,32 +1,18 @@
-const {
-  firestore,
-  admin
-} = require("../../helpers/firebase");
-const {
-  v4: uuidv4
-} = require("uuid");
-const {
-  encrypt
-} = require('../../helpers/crypto');
-const {
-  mailer
-} = require('../../helpers/mailer');
-const statuses = require('../enums/vacation-status');
+const { firestore, admin } = require("../../helpers/firebase");
+const { v4: uuidv4 } = require("uuid");
+const { encrypt } = require("../../helpers/crypto");
+const { mailer } = require("../../helpers/mailer");
+const statuses = require("../enums/vacation-status");
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-async function getUserById({
-  id
-}) {
+async function getUserById({ id }) {
   try {
     const userQuery = await firestore.collection("users").doc(id).get();
     const user = userQuery.data();
-    const {
-      password,
-      ...userWithoutPassword
-    } = user;
+    const { password, ...userWithoutPassword } = user;
 
     return {
       ...userWithoutPassword,
@@ -36,18 +22,36 @@ async function getUserById({
   }
 }
 
+async function getUsersByCompanyId({ companyId }) {
+  try {
+    const userQuery = await firestore
+      .collection("users")
+      .where("company.id", "==", companyId)
+      .get();
+    const users = userQuery.docs.map((d) => {
+      let user = d.data();
+      const { password, ...userWithoutPassword } = user;
+
+      return userWithoutPassword;
+    });
+
+    return {
+      users,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 async function getAllUsers() {
   try {
     const usersQuery = await firestore.collection("users").get();
-    const users = usersQuery.docs.map(d => {
+    const users = usersQuery.docs.map((d) => {
       let user = d.data();
-      const {
-        password,
-        ...userWithoutPassword
-      } = user;
+      const { password, ...userWithoutPassword } = user;
 
       return userWithoutPassword;
-    })
+    });
 
     return {
       users,
@@ -62,17 +66,23 @@ async function createUser({
   firstName,
   lastName,
   roleName,
-  companyName
+  companyName,
 }) {
   try {
     const id = uuidv4();
     const securityKey = uuidv4();
     let encryptedPassword = encrypt(id);
 
-    const roleQuery = await firestore.collection("roles").where("name", "==", roleName).get();
+    const roleQuery = await firestore
+      .collection("roles")
+      .where("name", "==", roleName)
+      .get();
     const role = roleQuery.docs[0].data();
 
-    const companyQuery = await firestore.collection("companies").where("name", "==", companyName).get();
+    const companyQuery = await firestore
+      .collection("companies")
+      .where("name", "==", companyName)
+      .get();
     const company = companyQuery.docs[0].data();
 
     await firestore.collection("users").doc(id).set({
@@ -87,21 +97,21 @@ async function createUser({
       isEmailConfirmed: false,
       securityKey,
       vacationLimit: company.yearVacationLimit,
-      vacations: []
+      vacations: [],
     });
 
     const mailOptions = {
       from: process.env.EMAIL_SERVICE_AUTH_EMAIL,
       to: email,
-      subject: 'Account Created Successfully',
-      text: 'Congrats!'
+      subject: "Account Created Successfully",
+      text: "Congrats!",
     };
 
     mailer.sendMail(mailOptions, function (error, info) {
       if (error) {
         console.log(error);
       } else {
-        console.log('Email sent: ' + info.response);
+        console.log("Email sent: " + info.response);
       }
     });
 
@@ -113,11 +123,21 @@ async function createUser({
   }
 }
 
-async function activateUser({
-  id,
-  password,
-  securityKey
-}) {
+async function deleteUser({ userId }) {
+  try {
+    await firestore.collection("users").doc(userId).update({
+      isActive: false,
+    });
+
+    return true;
+  } catch (error) {
+    console.log(error);
+  }
+
+  return false;
+}
+
+async function activateUser({ id, password, securityKey }) {
   try {
     const userQuery = await firestore.collection("users").doc(id).get();
     const user = userQuery.data();
@@ -128,28 +148,6 @@ async function activateUser({
       await firestore.collection("users").doc(id).update({
         password: encryptedPassword,
         isEmailConfirmed: true,
-      })
-
-      return true;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-
-  return false;
-}
-
-async function addUserVacation({
-  userId,
-  vacation
-}) {
-  try {
-    const userQuery = await firestore.collection("users").doc(userId).get();
-    const user = userQuery.data();
-
-    if (user) {
-      await firestore.collection("users").doc(userId).update({
-        vacations: admin.FieldValue.arrayUnion(vacation)
       });
 
       return true;
@@ -161,34 +159,70 @@ async function addUserVacation({
   return false;
 }
 
-async function updateUserVacation({
-  vacation
-}) {
+async function addUserVacation({ userId, vacation }) {
   try {
-    const userQuery = await firestore.collection("users").doc(vacation.userId).get();
+    const userQuery = await firestore.collection("users").doc(userId).get();
     const user = userQuery.data();
 
     if (user) {
-      let oldVacation = user.vacations.filter(vac => vac.id === vacation.id)[0];
+      await firestore
+        .collection("users")
+        .doc(userId)
+        .update({
+          vacations: admin.FieldValue.arrayUnion(vacation),
+        });
+
+      return true;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  return false;
+}
+
+async function updateUserVacation({ vacation }) {
+  try {
+    const userQuery = await firestore
+      .collection("users")
+      .doc(vacation.userId)
+      .get();
+    const user = userQuery.data();
+
+    if (user) {
+      let oldVacation = user.vacations.filter(
+        (vac) => vac.id === vacation.id
+      )[0];
       if (vacation.status === statuses.Accepted) {
-        await firestore.collection("users").doc(vacation.userId).update({
-          vacations: admin.FieldValue.arrayRemove(oldVacation)
-        });
+        await firestore
+          .collection("users")
+          .doc(vacation.userId)
+          .update({
+            vacations: admin.FieldValue.arrayRemove(oldVacation),
+          });
 
-        await firestore.collection("users").doc(vacation.userId).update({
-          vacationLimit: user.vacationLimit - vacation.days.length,
-          vacations: admin.FieldValue.arrayUnion(vacation)
-        });
+        await firestore
+          .collection("users")
+          .doc(vacation.userId)
+          .update({
+            vacationLimit: user.vacationLimit - vacation.days.length,
+            vacations: admin.FieldValue.arrayUnion(vacation),
+          });
       } else {
-        await firestore.collection("users").doc(vacation.userId).update({
-          vacations: admin.FieldValue.arrayRemove(oldVacation)
-        });
+        await firestore
+          .collection("users")
+          .doc(vacation.userId)
+          .update({
+            vacations: admin.FieldValue.arrayRemove(oldVacation),
+          });
 
-        await firestore.collection("users").doc(vacation.userId).update({
-          vacations: admin.FieldValue.arrayUnion(vacation)
-        });
+        await firestore
+          .collection("users")
+          .doc(vacation.userId)
+          .update({
+            vacations: admin.FieldValue.arrayUnion(vacation),
+          });
       }
-
 
       return true;
     }
@@ -205,5 +239,7 @@ module.exports = {
   getUserById,
   activateUser,
   addUserVacation,
-  updateUserVacation
+  updateUserVacation,
+  getUsersByCompanyId,
+  deleteUser,
 };
