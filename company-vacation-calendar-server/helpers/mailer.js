e9f0ccc490263175be0f33af4f63cdffa174544a
Simeon Mechkov <montishak@gmail.com>
2022-01-26 03:22:20 +0200

    fix(server): nodemailer old config

--- company-vacation-calendar-server/helpers/mailer.js
+++ company-vacation-calendar-server/helpers/mailer.js
@@ -4,10 +4,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_SERVICE_HOST,
-  port: process.env.EMAIL_SERVICE_PORT,
-  ignoreTLS: process.env.EMAIL_SERVICE_IGNORE_TLS,
-  secure: process.env.EMAIL_SERVICE_SECURE,
+  service: process.env.EMAIL_SERVICE_PROVIDER,
   auth: {
     user: process.env.EMAIL_SERVICE_AUTH_EMAIL,
     pass: process.env.EMAIL_SERVICE_AUTH_PASS,
